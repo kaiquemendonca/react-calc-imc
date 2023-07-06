4675689f592fc232d960e9fb12f8bf16cdf3ea52
Kaique Mendonca <kai_que15@hotmail.com>
2023-07-06 13:35:54 -0300

    primeiro commit

--- src/components/Square.tsx
+++
@@ -1,7 +0,0 @@
-export const Square = () =>{
-    return(
-        <div className="w-52 h-52 bg-orange-700 text-white flex items-center justify-center m-0">
-            <h1>Quadrado</h1>
-        </div>
-    );
-}
